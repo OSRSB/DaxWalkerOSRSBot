4113554b8a3ff51a3338332800d36b21e79fbc15
PhobicHD <null@null.com>
2023-07-06 12:48:15 -0600

    Make dax use Spell from the new PR

--- src/main/java/dax_api/shared/helpers/magic/Spell.java
+++ src/main/java/dax_api/shared/helpers/magic/Spell.java
@@ -2,28 +2,29 @@ package dax_api.shared.helpers.magic;
 
 import dax_api.shared.Pair;
 import net.runelite.api.Skill;
+import net.runelite.rsb.internal.globval.enums.MagicBook;
 import net.runelite.rsb.methods.Web;
 
 
 public enum  Spell implements Validatable  {
 
-    VARROCK_TELEPORT    (SpellBook.Type.STANDARD, 25, "Varrock Teleport",    new Pair<>(1, RuneElement.LAW), new Pair<>(3, RuneElement.AIR),     new Pair<>(1, RuneElement.FIRE)),
-    LUMBRIDGE_TELEPORT  (SpellBook.Type.STANDARD, 31, "Lumbridge Teleport",  new Pair<>(1, RuneElement.LAW), new Pair<>(3, RuneElement.AIR),     new Pair<>(1, RuneElement.EARTH)),
-    FALADOR_TELEPORT    (SpellBook.Type.STANDARD, 37, "Falador Teleport",    new Pair<>(1, RuneElement.LAW), new Pair<>(3, RuneElement.AIR),     new Pair<>(1, RuneElement.WATER)),
-    CAMELOT_TELEPORT    (SpellBook.Type.STANDARD, 45, "Camelot Teleport",    new Pair<>(1, RuneElement.LAW), new Pair<>(5, RuneElement.AIR)),
-    ARDOUGNE_TELEPORT   (SpellBook.Type.STANDARD, 51, "Ardougne Teleport",   new Pair<>(2, RuneElement.LAW), new Pair<>(2, RuneElement.WATER)),
-    KOUREND_TELEPORT	(SpellBook.Type.STANDARD, 69, "Kourend Castle Teleport",new Pair<>(2, RuneElement.LAW), new Pair<>(2, RuneElement.SOUL),new Pair<>(4, RuneElement.WATER), new Pair<>(5, RuneElement.FIRE))
+    VARROCK_TELEPORT    (SpellBook.Type.STANDARD, 25, MagicBook.Standard.VARROCK_TELEPORT, new Pair<>(1, RuneElement.LAW), new Pair<>(3, RuneElement.AIR),     new Pair<>(1, RuneElement.FIRE)),
+    LUMBRIDGE_TELEPORT  (SpellBook.Type.STANDARD, 31, MagicBook.Standard.LUMBRIDGE_TELEPORT, new Pair<>(1, RuneElement.LAW), new Pair<>(3, RuneElement.AIR),     new Pair<>(1, RuneElement.EARTH)),
+    FALADOR_TELEPORT    (SpellBook.Type.STANDARD, 37, MagicBook.Standard.FALADOR_TELEPORT, new Pair<>(1, RuneElement.LAW), new Pair<>(3, RuneElement.AIR),     new Pair<>(1, RuneElement.WATER)),
+    CAMELOT_TELEPORT    (SpellBook.Type.STANDARD, 45, MagicBook.Standard.CAMELOT_TELEPORT, new Pair<>(1, RuneElement.LAW), new Pair<>(5, RuneElement.AIR)),
+    ARDOUGNE_TELEPORT   (SpellBook.Type.STANDARD, 51, MagicBook.Standard.ARDOUGNE_TELEPORT, new Pair<>(2, RuneElement.LAW), new Pair<>(2, RuneElement.WATER)),
+    KOUREND_TELEPORT	(SpellBook.Type.STANDARD, 69, MagicBook.Standard.KOUREND_CASTLE_TELEPORT, new Pair<>(2, RuneElement.LAW), new Pair<>(2, RuneElement.SOUL),new Pair<>(4, RuneElement.WATER), new Pair<>(5, RuneElement.FIRE))
     ;
 
     private SpellBook.Type spellBookType;
     private int requiredLevel;
-    private String spellName;
+    private net.runelite.rsb.internal.globval.enums.Spell rsbSpell;
     private Pair<Integer, RuneElement>[] recipe;
 
-    Spell(SpellBook.Type spellBookType, int level, String spellName, Pair<Integer, RuneElement>... recipe){
+    Spell(SpellBook.Type spellBookType, int level, net.runelite.rsb.internal.globval.enums.Spell rsbSpell, Pair<Integer, RuneElement>... recipe){
         this.spellBookType = spellBookType;
         this.requiredLevel = level;
-        this.spellName = spellName;
+        this.rsbSpell = rsbSpell;
         this.recipe = recipe;
     }
 
@@ -31,12 +32,8 @@ public enum  Spell implements Validatable  {
         return recipe;
     }
 
-    public String getSpellName() {
-        return spellName;
-    }
-
     public boolean cast() {
-        return canUse() && Web.methods.magic.castSpell(Web.methods.magic.getSpell(getSpellName()));
+        return canUse() && Web.methods.magic.castSpell(rsbSpell);
     }
 
     @Override
